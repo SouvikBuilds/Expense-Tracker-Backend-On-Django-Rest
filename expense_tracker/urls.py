5eb4a60f6b5547a06e481c4b0138261d23e6a9c6
Souvik Chatterjee <souvikchatterjee@Souviks-MacBook-Air.local>
2025-09-24 21:49:28 +0530

    Admin Expelled From App Urls

--- expense_tracker/urls.py
+++ expense_tracker/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/',admin.site.urls),
     path('',views.home,name='home'),
     path('expenses/',views.get_all_expenses,name='get_all_expenses'),
     path('expenses/add/',views.add_expense,name='add_expense'),
